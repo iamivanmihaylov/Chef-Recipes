a4bf3ef568fc5c6443b36f6289b6a7c19df336e6
Ivan Angelov <61803518+ivan4o03@users.noreply.github.com>
2020-11-20 01:13:51 +0200

    Add control menu component

--- Angular/Chef-Recipes/src/app/home/home.module.ts
+++ Angular/Chef-Recipes/src/app/home/home.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { PageMessageComponent } from './page-message/page-message.component';
 import { SharedModule } from '../shared/shared.module';
+import { ControlMenuComponent } from './control-menu/control-menu.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, PageMessageComponent],
+  declarations: [HomeComponent, PageMessageComponent, ControlMenuComponent],
   imports: [
     CommonModule,
     SharedModule
