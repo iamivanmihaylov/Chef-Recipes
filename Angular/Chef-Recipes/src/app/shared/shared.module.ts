b19b53d90eec4233a1a68b5f7cf8b567ba406aac
Ivan Angelov <61803518+ivan4o03@users.noreply.github.com>
2020-11-25 18:07:12 +0200

    Add not found page

--- Angular/Chef-Recipes/src/app/shared/shared.module.ts
+++ Angular/Chef-Recipes/src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -7,7 +8,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
   declarations: [NotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
