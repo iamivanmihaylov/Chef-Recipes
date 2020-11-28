512470d5fdb7a5189fba26343c8c87f2f7df5dbe
Ivan Angelov <61803518+ivan4o03@users.noreply.github.com>
2020-11-29 00:31:53 +0200

    Fully add view recipe

--- Angular/Chef-Recipes/src/app/recipe/recipe-open/recipe-open.component.ts
+++ Angular/Chef-Recipes/src/app/recipe/recipe-open/recipe-open.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeOpenComponent implements OnInit {
 
+  showComments:boolean = false;
+  showIngredients:boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.addBottomBorder("menu-ingredients","3px","black");
   }
 
+  showIngredientsHandler():void{
+    this.addBottomBorder("menu-ingredients","3px","black");
+    this.removeBottomBorder("menu-comments","3px");
+    this.showIngredients = true;
+    this.showComments = false;
+  }
+
+  showCommentsHandler():void{
+    this.addBottomBorder("menu-comments","3px","black");
+    this.removeBottomBorder("menu-ingredients","3px");
+    this.showIngredients = false;
+    this.showComments = true;
+  }
+
+  addBottomBorder(elementClassName:string,width:string,color:string):void{
+    let element:HTMLElement = document.getElementsByClassName(elementClassName)[0] as HTMLElement;
+    element.style.borderBottom = `${width} solid ${color}`
+  }
+
+  removeBottomBorder(elementClassName:string,width:string):void{
+    let element:HTMLElement = document.getElementsByClassName(elementClassName)[0] as HTMLElement;
+    element.style.borderBottom = `${width} solid transparent`
+  }
 }
